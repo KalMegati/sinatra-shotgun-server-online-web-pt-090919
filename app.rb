c599c8d5b028d9070ea3c27cf47678383093ae9f
Srikalyan Megati <kalmegati@gmail.com>
2019-12-03 17:26:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "OH WHAT A FEAT /n /n HE'S ONE WITH THE MEAT"
+    "OH WHAT A FEAT \n \n HE'S ONE WITH THE MEAT"
   end
 
 end
